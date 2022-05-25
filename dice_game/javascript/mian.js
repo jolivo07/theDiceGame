8fd92eab0fcb66f9f5993436424a5b614b8c920f
wawandco <wawandco@MBP-de-wawandco.localdomain>
2022-05-25 15:29:40 -0500

    rename class and ids

--- dice_game/javascript/mian.js
+++ dice_game/javascript/mian.js
@@ -1,24 +1,24 @@
 function numberRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-document.getElementById("rollDice").addEventListener("click", () => {
-    document.getElementById("diceImageGif").classList.remove("d-none")
+document.getElementById("roll-dice").addEventListener("click", () => {
+    document.getElementById("dice-image-gif").classList.remove("d-none")
     document.getElementById("dice").classList.add("d-none")
     document.getElementById("result").classList.add("d-none")
 })
 document.getElementById("hold").addEventListener("click", () => {
-    document.getElementById("diceImageGif").classList.add("d-none")
+    document.getElementById("dice-image-gif").classList.add("d-none")
     document.getElementById("dice").classList.remove("d-none")
     document.getElementById("result").classList.remove("d-none")
-    let contOne = numberRandom(1, 7)
-    let contTwo = numberRandom(1, 7)
+    let diceNumberOne = numberRandom(1, 7)
+    let diceNumberTwo = numberRandom(1, 7)
     var hideDiceFacesPoints = (ids, value) => {
         var values = ids.split(",");
         for (var i in values)
             document.getElementById(values[i].replace(/\s/g, "")).style["visibility"] = value;
     };
     hideDiceFacesPoints("d1,d2,d3,d4,d5,d6,d7,di1,di2,di3,di4,di5,di6,di7", "hidden")
-    Faces = new Map([
+    faces = new Map([
         [1, [4]],
         [2, [1, 7]],
         [3, [1, 4, 7]],
@@ -26,14 +26,14 @@ document.getElementById("hold").addEventListener("click", () => {
         [5, [1, 3, 4, 5, 7]],
         [6, [1, 3, 2, 6, 5, 7]]
     ])
-    dOne = this.Faces.get(contOne);
-    dTwo = this.Faces.get(contTwo);
+    dOne = this.faces.get(diceNumberOne);
+    dTwo = this.faces.get(diceNumberTwo);
     dOne.forEach(value => {
         document.getElementById(`d${value}`).style["visibility"] = "visible";
     });
     dTwo.forEach(value => {
         document.getElementById(`di${value}`).style["visibility"] = "visible";
     });
-    let result = contOne + contTwo
+    let result = diceNumberOne + diceNumberTwo
     document.getElementById("result").innerHTML = result
 })
